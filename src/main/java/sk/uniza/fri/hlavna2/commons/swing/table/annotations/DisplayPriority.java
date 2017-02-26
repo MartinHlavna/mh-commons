56efa2d8d2902f2a67c3e4245d96108fc842118c
Martin Hlavňa <martin.hlavna@4q.sk>
2017-02-26 11:23:15 +0100

    Added reflective swing table model and annotations

---
+++ src/main/java/sk/uniza/fri/hlavna2/commons/swing/table/annotations/DisplayPriority.java
@@ -0,0 +1,13 @@
+package sk.uniza.fri.hlavna2.commons.swing.table.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface DisplayPriority {
+
+    public int priority();
+}
